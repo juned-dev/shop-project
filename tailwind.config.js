c82150240b3040fbc368a95bd634fc0b0a4fa8da
juned-dev <juned.dhanga@gmail.com>
2021-11-16 10:07:33 +0530

    style based ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+        'title' : ['"Bebas Neue", sans-serif'],
+        'body' : ['"Montserrat", sans-serif'],
+      },
+
+      container:{
+        DEFAULT: '1420px',
+        center: true,
+        padding: '1rem'
+      },
+
+      colors:{
+        primary:{
+          dark: '#59A13A',
+          light: '#74D44C',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
