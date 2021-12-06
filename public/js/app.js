16fcb916fc86e46ca022eafefbd55f6627740662
juned-dev <juned.dhanga@gmail.com>
2021-12-06 12:54:54 +0530

    mobile, tablate ui

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       speed: 500,
       fade: true,
       cssEase: 'linear',
-      prevArrow:"<img class='slick-prev' src='../images/icon_arrow_left_xl.svg' />",
-      nextArrow:"<img class='slick-next' src='../images/icon_arrow_right_xl.svg' />",
+      prevArrow:"<img class='slick-prev' src='images/icon_arrow_left_xl.svg' />",
+      nextArrow:"<img class='slick-next' src='images/icon_arrow_right_xl.svg' />",
     });
   });
